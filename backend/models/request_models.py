487a61ab367cfbb112ff2bc740ab08f5c4a695d9
Sarthak Singh <sarthaksingh1836@gmail.com>
2026-04-09 22:05:56 +0530

    change in structure

--- backend/models/request_models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Literal
-
-class TranspileRequest(BaseModel):
-    source_code: str
-    source_lang: Literal["python"] = "python"
-    target_lang: Literal["cpp", "javascript"]
